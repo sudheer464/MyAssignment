24c6d7c145f00d3c12b56d5fefb23fcda585b69c
Sudheer <sudheer.sk3@gmail.com>
2017-02-08 19:12:23 -0800

    Add index

--- mycontroller.py
+++ mycontroller.py
@@ -6,4 +6,15 @@ import json
 class Default(Controller):
     def GET(self):
        return 
-       
+
+    def POST(self, **kwargs):
+        return
+
+class Posts(Controller):
+    def GET(self):
+        conn = sqlite3.connect('blog.db')
+        c = conn.cursor()
+        c.execute('SELECT  * FROM posts') 
+        json_string = json.dumps(c.fetchall())
+        return json_string
+        conn.close()
