d5a50c770c8f6f9e0ddbd544691fad0d98219763
Sudheer <sudheer.sk3@gmail.com>
2017-02-08 19:18:27 -0800

    Post api

--- mycontroller.py
+++ mycontroller.py
@@ -18,3 +18,18 @@ class Posts(Controller):
         json_string = json.dumps(c.fetchall())
         return json_string
         conn.close()
+
+class Post(Controller):
+   def POST(self, **kwargs):
+        conn = sqlite3.connect('blog.db')
+        c = conn.cursor()
+        c.execute('SELECT Count(*) FROM posts') 
+        (post_id,) = c.fetchone()
+        row = ( post_id,kwargs['title'], kwargs['body'])
+        try:
+           c.execute('INSERT INTO posts (post_id,title,body) VALUES(?,?,?)', row ) 
+        except sqlite3.Error as e:
+            print "An error occurred:", e.args[0]
+        conn.commit()
+        conn.close()
+        return 'Posted blog id {}'.format(kwargs['title'])
